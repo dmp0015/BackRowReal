10feff1c26b5cc345232b68cf1d872587b623117
dmich <dmich@10.255.120.181>
2018-11-01 11:46:18 -0400

    Built program.

---
+++ src/backrow/Client.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package backrow;
+
+/**
+ *
+ * @author dmich
+ */
+public class Client {
+    String firstName;
+    String lastName;
+    String address;
+    String phoneNumber;
+    String meterType;
+    int accountNumber;
+}
